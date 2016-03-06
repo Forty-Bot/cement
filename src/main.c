798618fc33a7bce6489076070aa4188688c4de54
Sean Anderson <seanga2@gmail.com>
2016-03-05 19:41:51 -0500

    Fixed issue with fonts being too indescrete

--- src/main.c
+++ src/main.c
@@ -3,8 +3,11 @@
 #include <libtcod/libtcod.h>
 
 int main(void) {
-	TCOD_console_set_custom_font("res/dejavu10x10_gs_tc.png", TCOD_FONT_LAYOUT_TCOD, 0, 0);
+	// Initialize the console
+	TCOD_console_set_custom_font("res/dejavu16x16_gs_tc.png", TCOD_FONT_LAYOUT_TCOD | TCOD_FONT_TYPE_GREYSCALE, 0, 0);
 	TCOD_console_init_root(80, 50, "Cement", false, TCOD_RENDERER_SDL);
+	TCOD_sys_set_fps(20);
+	// Main loop
 	while(!TCOD_console_is_window_closed()) {
 		TCOD_sys_check_for_event(TCOD_EVENT_KEY_PRESS, NULL, NULL);
 		TCOD_console_clear(NULL);
