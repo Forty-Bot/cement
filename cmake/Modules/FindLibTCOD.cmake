6084341d08ffc5a45e07d0d8c348fef1b57269e5
Sean Anderson <seanga2@gmail.com>
2016-03-06 15:04:32 -0500

    Include libtcod explicitly (hopefully fixes ycm)

--- cmake/Modules/FindLibTCOD.cmake
+++ cmake/Modules/FindLibTCOD.cmake
@@ -9,13 +9,13 @@ find_path(LIBTCOD_INCLUDE_DIR libtcod.h PATH_SUFFIXES libtcod)
 
 find_library(LIBTCOD_LIBRARY NAMES libtcod tcod)
 
-set(LIBTCOD_LIBRARIES ${LIBTCOD_LIBRARY} )
-set(LIBTCOD_INCLUDE_DIRS ${LIBTCOD_INCLUDE_DIR} )
+set(LIBTCOD_LIBRARIES ${LIBTCOD_LIBRARY})
+set(LIBTCOD_INCLUDE_DIRS ${LIBTCOD_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LIBTCOD_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(LibTCOD DEFAULT_MSG
+find_package_handle_standard_args(LIBTCOD DEFAULT_MSG
                                   LIBTCOD_LIBRARY LIBTCOD_INCLUDE_DIR)
 
 mark_as_advanced(LIBTCOD_INCLUDE_DIR LIBTCOD_LIBRARY)
